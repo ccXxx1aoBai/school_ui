803905cf9620a616dfbd4478032ced0cb2f82b5b
嘵白 <9748766+lwb0118@user.noreply.gitee.com>
2022-11-02 23:15:27 +0800

    选课功能

---
+++ src/pages/edu/courses.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    data() {
+      return {
+        
+      }
+    },
+    computed: {
+      date() {
+
+      }
+    }
+  }
+</script>
+
+<style lang="" scoped>
+  
+</style>
